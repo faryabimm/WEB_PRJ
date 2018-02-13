0e6f8970f4affcc2a194de7d07f01374478767e0
Reza Taheri <h.rezataheri@gmail.com>
2018-02-13 23:36:43 +0330

    login-page-ui

--- src/containers/LogIn/LoginPage.js
+++ src/containers/LogIn/LoginPage.js
@@ -5,20 +5,24 @@ import Dice from "../Dice/Dice";
 class LoginPage extends Component{
     render(){
         const e1 = (
-            <div className='main-container'>
-                <div className='background-of-elements'>
-                    <div className='two-inputs-container' >
-                        {/*<p className='input-upper-text'>Username:</p>*/}
-                        <input type='text' className='username-input vertical-alignment input-style' id='UserName' placeholder='Username'/>
-                        {/*<p className='input-upper-text'>Password:</p>*/}
-                        <input type='password' className='password-input vertical-alignment input-style' id='PassWord' placeholder='Password'/>
-                    </div>
-                    <div className='buttons-container'>
-                        <button className='Login-button button-style'>Login</button>
-                        <button className='Register-button button-style'>Register</button>
+            <div className='background-image' >
+                <div className='background-overlay'>
+                    <div className='main-container'>
+                        <div className='background-of-elements'>
+                            <div className='two-inputs-container' >
+                                {/*<p className='input-upper-text'>Username:</p>*/}
+                                <input type='text' className='username-input vertical-alignment input-style' id='UserName' placeholder='Username'/>
+                                {/*<p className='input-upper-text'>Password:</p>*/}
+                                <input type='password' className='password-input vertical-alignment input-style' id='PassWord' placeholder='Password'/>
+                            </div>
+                            <div className='buttons-container'>
+                                <button className='Login-button button-style'>Login</button>
+                                <button className='Register-button button-style'>Register</button>
+                            </div>
+                        </div>
+                        <Dice/>
                     </div>
                 </div>
-                <Dice/>
             </div>
         )
 
